9655e53aca5b9c3fb55dd7e54cca8428d1f2af76
Xregion <perri_kyle@ymail.com>
2017-06-20 14:43:09 -0700

    Added Event Class for Feed Fragment List

--- app/src/main/java/com/adc/mycircle/items/EventContent.java
+++ app/src/main/java/com/adc/mycircle/items/EventContent.java
@@ -1,72 +1,45 @@
-package com.adc.mycircle.dummy;
+package com.adc.mycircle.items;
+
+import android.graphics.Color;
+import android.media.Image;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Helper class for providing sample content for user interfaces created by
- * Android template wizards.
- * <p>
- * TODO: Replace all uses of this class before publishing your app.
- */
-public class DummyContent {
+public class EventContent {
 
-    /**
-     * An array of sample (dummy) items.
-     */
-    public static final List<DummyItem> ITEMS = new ArrayList<DummyItem>();
+    public static final List<Event> ITEMS = new ArrayList<Event>();
 
-    /**
-     * A map of sample (dummy) items, by ID.
-     */
-    public static final Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
-
-    private static final int COUNT = 25;
-
-    static {
-        // Add some sample items.
-        for (int i = 1; i <= COUNT; i++) {
-            addItem(createDummyItem(i));
-        }
-    }
-
-    private static void addItem(DummyItem item) {
-        ITEMS.add(item);
-        ITEM_MAP.put(item.id, item);
-    }
-
-    private static DummyItem createDummyItem(int position) {
-        return new DummyItem(String.valueOf(position), "Item " + position, makeDetails(position));
-    }
-
-    private static String makeDetails(int position) {
-        StringBuilder builder = new StringBuilder();
-        builder.append("Details about Item: ").append(position);
-        for (int i = 0; i < position; i++) {
-            builder.append("\nMore details information here.");
-        }
-        return builder.toString();
+    // TODO: Allow user to set up variables for events
+    private static Event createEvent(int position, String groupName, String eventTitle, String details, Color backgroundColor, Image backgroundImage) {
+        return new Event(String.valueOf(position), groupName, eventTitle, details, backgroundColor, backgroundImage);
     }
 
     /**
-     * A dummy item representing a piece of content.
+     * An Event item that will be created by groups
      */
-    public static class DummyItem {
+    public static class Event {
         public final String id;
-        public final String content;
+        public final String groupName;
+        public final String eventTitle;
         public final String details;
+        public final Color backgroundColor;
+        public final Image backgroundImage;
 
-        public DummyItem(String id, String content, String details) {
+        public Event(String id, String groupName, String eventTitle, String details, Color backgroundColor, Image backgroundImage) {
             this.id = id;
-            this.content = content;
+            this.groupName = groupName;
+            this.eventTitle = eventTitle;
             this.details = details;
+            this.backgroundColor = backgroundColor;
+            this.backgroundImage = backgroundImage;
         }
 
         @Override
         public String toString() {
-            return content;
+            return eventTitle;
         }
     }
 }
